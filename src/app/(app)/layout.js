6218c6677870988ec3973e01823f082d79fe5ff1
gitnyasha <marshall.chikari@gmail.com>
2024-05-14 22:46:53 +0200

    new design

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -4,6 +4,8 @@
 import { useAuth } from '@/hooks/auth'
 import Navigation from '@/app/(app)/Navigation'
 import Loading from '@/app/(app)/Loading'
+import '@/app/dashboard.css'
+import Header from '@/app/(app)/Header'
 
 const AppLayout = ({ children, header }) => {
     const { user } = useAuth({ middleware: 'auth' })
@@ -13,10 +15,12 @@ const AppLayout = ({ children, header }) => {
     }
 
     return (
-        <div className="App">
-            <Navigation user={user} />
-
-            <main>{children}</main>
+        <div className="flex h-screen overflow-hidden">
+            <Navigation />
+            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                <Header user={user} />
+                <main>{children}</main>
+            </div>
         </div>
     )
 }
---
+++ src/components/WhiteLogo.js
@@ -0,0 +1,7 @@
+const WhiteLogo = props => (
+    <div className="nav-logo-container">
+        <img src="images/logo-on-white.png" {...props} />
+    </div>
+)
+
+export default WhiteLogo
