bba7bef52fd66256c3602e8cbb4cc922cc6b2faa
gitnyasha <marshall.chikari@gmail.com>
2024-05-15 19:58:07 +0200

    .

--- src/app/(app)/Header.js
+++ src/app/(app)/Header.js
@@ -150,9 +150,7 @@ const Header = ({ user }) => {
                                         </svg>
                                     </a>
                                 }>
-                                <div
-                                    x-show="dropdownOpen"
-                                    className="absolute -right-27 mt-2.5 flex h-90 w-75 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark sm:right-0 sm:w-80">
+                                <div className="absolute -right-27 mt-2.5 flex h-90 w-75 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark sm:right-0 sm:w-80">
                                     <div className="px-4.5 py-3">
                                         <h5 className="text-sm font-medium text-bodydark2">
                                             Notification
