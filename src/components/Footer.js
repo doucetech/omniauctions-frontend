4198d666149dfa73e5663028dad4781d5fe933b3
Rodgerzmarufu <rodgerssimbarashemarufu@gmail.com>
2024-06-15 14:54:43 +0200

    footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,36 +10,36 @@ const Footer = () => {
             <div className="footer-items-flex">
                 <div className="get-started">
                     <h2>Getting Started</h2>
-                    <a href="">All Categories</a>
-                    <a href="">How to bid</a>
-                    <a href="">FAQs</a>
+                    <a href="#">All Categories</a>
+                    <a href="#">How to bid</a>
+                    <a href="#">FAQs</a>
                 </div>
                 <div className="useful-links">
                     <h2>Useful Links</h2>
-                    <a href="">Contact Us</a>
-                    <a href="">Log in</a>
-                    <a href="">Register</a>
+                    <a href="#">Contact Us</a>
+                    <a href="#">Log in</a>
+                    <a href="#">Register</a>
                 </div>
                 <div className="need-more">
                     <h2>Need More info?</h2>
-                    <a href="">About Us</a>
-                    <a href="">Sell with us</a>
+                    <a href="#">About Us</a>
+                    <a href="#">Sell with us</a>
                 </div>
                 <div className="follow">
                     <h2>Follow Us</h2>
-                    <a>
+                    <a href='#'>
                         <i className="icon-youtube"></i>
                     </a>
-                    <a>
+                    <a href='#'>
                         <i className="icon-linkedin"></i>
                     </a>
-                    <a>
+                    <a href='#'>
                         <i className="icon-instagram"></i>
                     </a>
-                    <a>
+                    <a href='#'>
                         <i className="icon-twitter"></i>
                     </a>
-                    <a>
+                    <a href='#'>
                         <i className="icon-facebook"></i>
                     </a>
                 </div>
